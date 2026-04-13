cee099598dd883010b4c2263e9600806103f1444
Taylor Mendes <taylormendes927@gmail.com>
2026-04-13 14:48:30 -0400

    Update define_states.py

--- src/define_states.py
+++ src/define_states.py
@@ -1,1 +1,9 @@
+STATES = ["U", "R", "A", "I", "D"]
 
+STATE_LABELS = {
+    "U": "Unregistered",
+    "R": "Registered but inactive",
+    "A": "Active voter",
+    "I": "Inactive voter",
+    "D": "Disenfranchised / removed"
+}
