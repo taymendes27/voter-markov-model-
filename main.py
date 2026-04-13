9c66a507560e9f6b8216f05efddf4dc6f73cceca
Taylor Mendes <taylormendes927@gmail.com>
2026-04-13 14:49:35 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
+from src.transition_matrix import get_transition_matrix, validate_transition_matrix
 
+P = get_transition_matrix()
+print("Transition matrix:")
+print(P)
+
+print("\nValid stochastic matrix?", validate_transition_matrix(P))
